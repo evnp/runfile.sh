a9ce22bed86cea38adaeabab6ce467d3e2cbc1b5
Evan Purcer <ep@e2.gg>
2024-08-26 08:20:40 -0700

    feature: verbose

--- run.sh
+++ run.sh
@@ -5,17 +5,30 @@
 set -euo pipefail
 
 function run {
-	local makefile
-	makefile="$( mktemp )"
+	local mf vb make_args
 
-cat <<EOF > "${makefile}"
+	mf="$( mktemp )" # makefile
+	vb="@"           # verbose - @ causes make to execute commands silently
+	make_args=( --makefile "${mf}" ) # arguments that will be passed on to make
+
+	for arg in "$@"
+	do
+		if [[ "${arg}" == '--verbose' || "${arg}" == '-v' ]]
+		then
+			vb="" # verbose - remove @ so that make prints commands before executing them
+		else
+			make_args+=( "${arg}" )
+		fi
+	done
+
+cat <<EOF > "${mf}"
 h help :: .usage
 
 $(
-	sed -Ee 's~^[[:space:]]*~\t@~' \
-			-e 's~^\t@([a-zA-Z0-9])([a-zA-Z0-9]+) :: (.*)$~\1 \1\2 :: # \3~' \
-			-e "s~^\t@run ~\t@make --makefile ${makefile} ~" \
-			-e 's~@$~~' \
+	sed -Ee "s~^[[:space:]]*~\t${vb}~" \
+			-e "s~^\t${vb}([a-zA-Z0-9])([a-zA-Z0-9]+) :: (.*)$~\1 \1\2 :: # \3~" \
+			-e "s~^\t${vb}run ~\t${vb}make --makefile ${mf} ~" \
+			-e "s~\t${vb}$~\t~" \
 		Runfile
 )
 
@@ -28,15 +41,15 @@ EOF
 		if ! [[ " $* " == *' --overwrite-makefile '* ]] && [[ -f 'Makefile' ]]
 		then
 			echo "Makefile already exists. Run again with --overwrite-makefile to overwrite."
-			rm "${makefile}"
+			rm "${mf}"
 			exit 1
 		fi
-		sed -E "s~@make --makefile ${makefile} ~@make ~" "${makefile}" > ./Makefile
+		sed -E "s~\t${vb}make --makefile ${mf} ~\t${vb}make ~" "${mf}" > ./Makefile
 	else
-		make --makefile "${makefile}" "$@"
+		make "${make_args[@]}"
 	fi
 
-	rm "${makefile}"
+	rm "${mf}"
 	exit 0
 }
 
