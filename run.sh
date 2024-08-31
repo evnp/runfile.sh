06356d95997dd5eb740174f452cee9f94e6a3396
Evan Purcer <ep@e2.gg>
2024-08-31 08:54:53 -0700

    code cleanup and comments

--- run.sh
+++ run.sh
@@ -62,11 +62,7 @@ function smartcase-file() { local name=''
 	echo "${name}"
 }
 
-function print-file() {
-	cat "$( smartcase-file "$1" )"
-}
-
-function edit-file() { local name=''
+function edit-file-smartcase() { local name=''
 	name="$( smartcase-file "$1" )"
 	if [[ " $* " != *' --noedit '* ]]
 	then
@@ -76,55 +72,75 @@ function edit-file() { local name=''
 	fi
 }
 
+function print-file-smartcase() {
+	cat "$( smartcase-file "$1" )"
+}
+
+function print-makefile() {
+	sed -E "s|\t${vb}make --makefile ${mf} |\t${vb}make |" "$@"
+}
+
 function cd-to-nearest-file() { local lower='' upper='' title=''
 	lower="$( lowercase-file "$1" )"
 	upper="$( uppercase-file "$1" )"
 	title="$( titlecase-file "$1" )"
 	while ! [[ -f "./${lower}" || -f "./${upper}" || -f "./${title}" ]]
 	do
-		if [[ "$( pwd )" == "$HOME" ]]
+		if ! [[ "$( pwd )" == "$HOME" ]]
 		then
+			cd ..
+		else
 			echo "No ${title} found."
 			echo "Use `run --create-${lower}` to create one here."
+			# Note: This message pertains to the user's shell which _won't_ have
+			# changed directory, because this script's main function uses a subshell.
+			# So there's no need to change directory back to where we started here.
 			exit 1
 		fi
-		cd ..
 	done
 }
 
 function main() ( set -euo pipefail; local mf='' vb='' make_args=()
 	# Handle various optional actions:
 	[[ " $* " == *' --create-runfile '* ]] && \
-		create-runfile "$@" && edit-file runfile --confirm "$@" && exit 0
+		create-runfile "$@" && edit-file-smartcase runfile --confirm "$@" && exit 0
 	[[ " $* " == *' --print-runfile '* ]] && \
-		cd-to-nearest-file runfile && print-file runfile && exit 0
+		cd-to-nearest-file runfile && print-file-smartcase runfile && exit 0
 	[[ " $* " == *' --edit-runfile '* ]] && \
-		cd-to-nearest-file runfile && edit-file runfile "$@" && exit 0
+		cd-to-nearest-file runfile && edit-file-smartcase runfile "$@" && exit 0
 	[[ " $* " == *' --edit-makefile '* ]] && \
-		cd-to-nearest-file makefile && edit-file makefile "$@" && exit 0
+		cd-to-nearest-file makefile && edit-file-smartcase makefile "$@" && exit 0
 
-	cd-to-nearest-file Runfile
+	# If no runfile in current dir, navigate up looking for one until we reach $HOME:
+	cd-to-nearest-file runfile
 
-	mf="$( mktemp )" # makefile
-	vb="@"					 # verbose - @ causes make to execute commands silently
-	make_args=( --makefile "${mf}" ) # arguments that will be passed on to make
+	mf="$( mktemp )"	# Temporary makefile which we will pass to make.
+	vb="@"						# Verbose - @ causes make to execute commands silently.
+	make_args=()			# Arguments that will be passed on to make.
+	make_sepr=" :: "	# Use double-colon to separate targets from descriptions in
+										# Makefile. This allows running targets without dependencies,
+										# similar to .PHONY rules.
 
+	# Handle these args which we don't want to pass on to make:
+	# --verbose / -v
 	for arg in "$@"
 	do
 		if [[ "${arg}" == '--verbose' || "${arg}" == '-v' ]]
 		then
-			vb="" # verbose - remove @ so that make prints commands before executing them
+			vb="" # (verbose) Remove @ so that make prints commands before executing them.
 		else
 			make_args+=( "${arg}" )
 		fi
 	done
 
+############################################
+# Construct temporary Makefile from Runfile:
 cat <<EOF > "${mf}"
 h help :: .usage
 
 $(
 	sed -Ee "s|^[[:space:]]*|\t${vb}|" \
-			-e "s|^\t${vb}([a-zA-Z0-9_-])([a-zA-Z0-9_-]+): (.*)$|\1 \1\2 :: # \3|" \
+			-e "s|^\t${vb}([a-zA-Z0-9_-])([a-zA-Z0-9_-]+): (.*)$|\1 \1\2${make_sepr}# \3|" \
 			-e "s|^\t${vb}run |\t${vb}make --makefile ${mf} |" \
 			-e "s|\t${vb}$|\t|" \
 		Runfile
@@ -133,29 +149,36 @@ $(
 .usage:
 	@grep -E "^[^@]*:.*#" \$(MAKEFILE_LIST) | sed -E "s/(.*):(.*):.*#(.*)/	\\1·\\2\\3/"
 EOF
+# Done with temporary Makefile construction.
+############################################
 
-	if [[ " $* " == *' --write-makefile '* || " $* " == *' --overwrite-makefile '* ]]
+	# --create-makefile    :: Write generated Makefile then exit.
+	# --overwrite-makefile :: Can be used to overwrite when Makefile already exists.
+	if [[ " $* " == *' --create-makefile '* || " $* " == *' --overwrite-makefile '* ]]
 	then
 		if ! [[ " $* " == *' --overwrite-makefile '* ]] && [[ -f 'Makefile' ]]
 		then
-			echo "Makefile already exists. Run again with --overwrite-makefile to overwrite."
+			echo "Makefile already exists."
+			echo "Use `run --overwrite-makefile` to overwrite."
 			rm "${mf}"
 			exit 1
+		else
+			print-makefile "${mf}" > ./Makefile
+			rm "${mf}"
+			exit 0
 		fi
-		sed -E "s|\t${vb}make --makefile ${mf} |\t${vb}make |" "${mf}" > ./Makefile
-		rm "${mf}"
-		exit 0
 	fi
 
-	# Print generated Makefile then exit:
+	# --print-makefile :: Print generated Makefile then exit.
 	if [[ " $* " == *' --print-makefile '* ]]
 	then
-		sed -E "s|\t${vb}make --makefile ${mf} |\t${vb}make |" "${mf}"
+		print-makefile "${mf}"
 		rm "${mf}"
 		exit 0
 	fi
 
-	make "${make_args[@]}"
+	# Main Path :: Invoke make with generated makefile and all other arguments.
+	make --makefile "${mf}" "${make_args[@]}"
 	rm "${mf}"
 	exit 0
 )
